561035d98a39f64f6400776e5b4f55cb247b2221
Victor Ribeiro <devjvribeiro@gmail.com>
2021-03-14 16:20:38 -0300

    Updated ALM shortcode

--- page-portfolio.php
+++ page-portfolio.php
@@ -149,18 +149,20 @@ $search = $_GET[ 'q' ];
         <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/custom/ajax-load-more.custom.css" defer />
 
         <?= do_shortcode('
-            [ajax_load_more 
-              container_type="div" 
-              post_type="projetos" 
-              posts_per_page="6" 
-              offset="0" 
-              pause="false" 
-              images_loaded="true" 
-              scroll="true" 
-              progress_bar="true" 
-              progress_bar_color="868ff3" 
-              button_label="Carregar mais" 
-              button_loading_label="Carregando..." 
+            [ajax_load_more
+              tag="'. $tag .'"
+              search="'. $search .'"
+              post_type="projetos"
+              scroll="true"
+              posts_per_page="6"
+              container_type="div"
+              offset="0"
+              pause="false"
+              images_loaded="true"
+              orderby="menu_order"
+              button_loading_label="Carregando..."
+              progress_bar_color="868ff3"
+              button_label="Carregar mais"
               button_done_label="Isso é tudo :)"
             ]'); ?>
       </div>
