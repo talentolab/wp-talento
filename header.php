8fc3c67a8e7ebe597ef169b70b70a7116edb6137
Victor Ribeiro <devjvribeiro@gmail.com>
2021-03-15 02:14:33 -0300

    Removed Material Icons to keep Font Awesome only

--- header.php
+++ header.php
@@ -20,8 +20,6 @@
 
   <link rel="profile" href="https://gmpg.org/xfn/11">
 
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
   <link rel="apple-touch-icon" sizes="180x180" href="<?= get_template_directory_uri(); ?>/img/icons/favicon/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="<?= get_template_directory_uri(); ?>/img/icons/favicon/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="<?= get_template_directory_uri(); ?>/img/icons/favicon/favicon-16x16.png" />
