80e922dd063f1cf8122291df37722827d1964a37
Victor Ribeiro <devjvribeiro@gmail.com>
2021-06-25 00:31:09 -0300

    Updated contact page

--- page-contact.php
+++ page-contact.php
@@ -7,19 +7,34 @@
 get_header();
 ?>
 
-<main class="background-blob">
-  <div class="block padding-y-larger">
-    <div class="container-medium">
+<main class="margin-top-large">
+  <div class="block primary-bg wave-bg--sine-2">
+    <div class="padding-y-larger">
       <div class="no-padding-x">
         <div class="row no-margin">
-          <div class="col s12 l6 padding-small budget--left-panel hide-on-med-and-down">
-            <div class="card-custom-image truck wow fadeInUpSmall" data-wow-delay=".1s"></div>
+          <div class="col s12 m1 l1 xl2 hide-on-small-only"></div>
+
+          <div class="col s12 l5 xl5 padding-small budget--left-panel hide-on-med-and-down">
+            <div class="contact-ilustration wow fadeInLeftSmall" data-wow-delay=".2s"></div>
+
+            <div class="margin-top-larger text-center">
+              <a href="#" class="btn green">
+                <i class="fab fa-whatsapp left"></i>
+                Chama no WhatsApp
+              </a>
+
+              <a href="#" class="btn margin-left-smaller">
+                <i class="fas fa-phone-alt left"></i>
+                Ligar
+              </a>
+            </div>
           </div>
-          <div class="col s12 l6">
-            <div class="card padding-medium border-radius-large wow fadeInUpSmall low-shadow" data-wow-delay=".1s">
+
+          <div class="col s12 m10 l5 xl4">
+            <div class="card contact-page--form padding-medium border-radius-large wow fadeInUpSmall low-shadow" data-wow-delay=".1s">
               <div class="card-content no-padding">
 
-                <h2 class="budget-area-title">Entre em contato</h2>
+                <h2 class="accent-color">Mande um e-mail</h2>
 
                 <p class="text-medium subtitle-color margin-y-medium">
                   Use o formulário abaixo para solicitar um orçamento ou
@@ -27,44 +42,46 @@ get_header();
                 </p>
 
                 <form>
-                  <div class="row row margin-bottom-small wow fadeInUpSmall" data-wow-delay=".2s">
+                  <div class="row margin-bottom-small wow fadeInUpSmall" data-wow-delay=".2s">
                     <div class="input-field col s12 no-margin">
-                      <input name="name" id="name" type="text" class="validate no-margin-y" required><br>
-                      <label for="name">Nome</label>
+                      <input name="name" id="name" type="text" class="validate padding-y-smaller" required><br>
+                      <label for="name" class="text-medium">Nome</label>
                     </div>
                   </div>
 
-                  <div class="row row margin-bottom-small wow fadeInUpSmall" data-wow-delay=".3s">
+                  <div class="row margin-bottom-small wow fadeInUpSmall" data-wow-delay=".3s">
                     <div class="input-field col s12 no-margin">
-                      <input name="email" id="email" type="email" class="validate no-margin-y" required><br>
-                      <label for="email">E-mail</label>
+                      <input name="email" id="email" type="email" class="validate padding-y-smaller" required><br>
+                      <label for="email" class="text-medium">E-mail</label>
                     </div>
                   </div>
 
-                  <div class="row row margin-bottom-small wow fadeInUpSmall" data-wow-delay=".4s">
+                  <div class="row margin-bottom-small wow fadeInUpSmall" data-wow-delay=".4s">
                     <div class="input-field col s12 no-margin">
-                      <input name="phone" id="phone" type="text" class="validate no-margin-y" required><br>
-                      <label for="phone">Telefone / Celular</label>
+                      <input name="phone" id="phone" type="text" class="validate padding-y-smaller" required><br>
+                      <label for="phone" class="text-medium">Telefone / Celular</label>
                     </div>
                   </div>
 
-                  <div class="row row margin-bottom-small wow fadeInUpSmall" data-wow-delay=".5s">
+                  <div class="row margin-bottom-small wow fadeInUpSmall" data-wow-delay=".5s">
                     <div class="input-field col s12 no-margin">
-                      <textarea name="message" id="message" class="materialize-textarea validate no-margin-y"
+                      <textarea name="message" id="message" class="materialize-textarea validate padding-y-smaller"
                         required></textarea><br>
-                      <label for="message">Sua mensagem</label>
+                      <label for="message" class="text-medium">Sua mensagem</label>
                     </div>
                   </div>
 
-                  <div class="row row no-margin">
+                  <div class="row no-margin">
                     <div class="input-field col s12 no-margin no-padding">
-                      <input type="submit" value="Enviar" class="btn primary-bg shadow-0">
+                      <input type="submit" value="Enviar mensagem" class="btn primary-bg shadow-0">
                     </div>
                   </div>
                 </form>
               </div>
             </div>
           </div>
+
+          <div class="col s12 m1 l1 xl2 hide-on-small-only"></div>
         </div>
       </div>
     </div>
