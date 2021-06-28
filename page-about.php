adeba374e7e99bccbb04b71ce34394290db6fe65
Victor Ribeiro <devjvribeiro@gmail.com>
2021-06-28 01:34:27 -0300

    Updated About page

--- page-about.php
+++ page-about.php
@@ -28,29 +28,169 @@ get_header();
   </div>
 </header>
 
+<main>
+  <div class="block text-color padding-y-larger primary-bg wave-bg--sine-2">
+    <div class="container-large row valign-wrapper flex-on-med-and-up">
+      <div class="col s12 m4">
+        <div class="card text-middle border-radius-large low-shadow">
+          <div class="card-content padding-large">
+            <ul class="no-margin">
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-map-marked-alt margin-right-small accent-color"></i> Conheça a nossa empresa
+              </li>
+
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-check-double margin-right-small accent-color"></i> Encontre soluções
+              </li>
+
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-pen-nib margin-right-small accent-color"></i> Veja outros trabalhos
+              </li>
+
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-compass margin-right-small accent-color"></i> Tenha referências
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="card text-middle border-radius-large low-shadow">
+          <div class="card-content padding-large">
+            <h2 class="no-margin-top">Design Empresarial</h2>
+            <ul class="no-margin">
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-star margin-right-small accent-color"></i> Aumente a visibilidade de sua marca
+              </li>
+
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-star margin-right-small accent-color"></i> Aumente suas vendas através de seu site
+              </li>
+
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-star margin-right-small accent-color"></i> Torne sua empresa funcional
+              </li>
+
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-star margin-right-small accent-color"></i> Acerte na propaganda
+              </li>
+
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-star margin-right-small accent-color"></i> Torne sua empresa mais dinâmica
+              </li>
+
+              <li class="margin-bottom-small display-flex">
+                <i class="fas fa-star margin-right-small accent-color"></i> Conheça e se relacione com o seu público
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="card padding-large text-middle border-radius-large low-shadow">
+          <p>Ao longo desses anos, foram muitos desafios. Participamos de grandes 
+            eventos e premiações de marcas que nos dão muito orgulho.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="block primary-bg padding-y-large">
+    <div class="row valign-wrapper flex-on-med-and-up no-margin padding-x-medium">
+      <div class="col s12 m1 l2"></div>
+
+      <div class="col s12 m6 l3 text-middle no-padding">
+        <div>
+          <h2 class="white-text">Anos de mercado</h2>
+            <p class="white-text">Atuamos no mercado há <strong>treze anos</strong> oferecendo e implantando projetos que valorizam o design e a inovação.</p>
+            
+            <a href="#" class="btn button-bg right">
+              Alguma ação
+              <i class="fas fa-arrow-right text-small"></i>
+            </a>
+        </div>
+      </div>
+
+      <div class="col s12 m6 l9 padding-medium">
+        <div class="card no-padding text-middle hoverable border-radius-larger">
+          <div class="video-container">
+            <iframe width="853" height="460" src="//www.youtube.com/embed/6Mn3gaHr_Fc?rel=0" frameborder="0"
+              allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
-<div id="content" class="site-content gradient-bg">
+  <div class="block light-purple-bg padding-top-large wave-bg--sine text-color" style="z-index: 1;">
+    <div class="row valign-wrapper flex-on-med-and-up no-margin">
+      <div class="col s12 m1 l2"></div>
+
+      <div class="col s12 m5 l4 padding-x-medium">
+        <div class="card text-middle border-radius-large low-shadow">
+          <div class="card-content padding-medium">
+            <h2 class="primary-color">Reconhecimento</h2>
+            <p>Ao longo desses anos, foram muitos desafios. Participamos de grandes 
+              eventos e premiações de marcas que nos dão muito orgulho.</p>
+
+            <div class="divider margin-top-medium margin-bottom-small"></div>
+            
+            <a href="#" class="btn button-bg right">
+              Alguma ação
+              <i class="fas fa-arrow-right text-small margin-left-small"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m5 l4">
+        <div class="meet-talento-ilustration wow fadeInLeftSmall overlap-bottom-larger hide-on-med-and-down" data-wow-delay=".2s"></div>
+      </div>
+
+      <div class="col s12 m1 l2"></div>
+    </div>
+  </div>
+
+
+
+  <div class="block light-purple-bg padding-y-large wave-bg--8">
+    <div class="row valign-wrapper flex-on-med-and-up">
+      <div class="col s12 m1 l2"></div>
+
+      <div class="col s12 m5 l4">
+        <div class="meet-talento-ilustration wow fadeInLeftSmall" data-wow-delay=".2s"></div>
+      </div>
+
+      <div class="col s12 m5 l4 padding-x-medium">
+        <div class="card text-middle border-radius-large low-shadow">
+          <div class="card-content padding-medium">
+            <h2 class="primary-color">Das micro às grandes</h2>
+
+            <p>
+              Buscamos por desafios para transformá-los em inovação. Por isso, projetamos para pequenas, 
+              médias e grandes empresas com sensibilidade e responsabilidade.
+            </p>
+
+            <div class="divider margin-top-medium margin-bottom-small"></div>
+            
+            <a href="#" class="btn button-bg right">
+              Alguma ação
+              <i class="fas fa-arrow-right text-small margin-left-small"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m1 l2"></div>
+    </div>
+  </div>
+
+
+
+
   <div class="block">
-    <div class="container-small padding-bottom-larger">
-      <h1 class="page-title text-center wow fadeInUpSmall" data-wow-delay=".5s">
-        <?php the_title(); ?>
-      </h1>
-
-      <h3 class="page-subtitle wow fadeInUpSmall" data-wow-delay=".7s">
-        <?php the_excerpt(); ?>
-      </h3>
-
-      <?php 
-        if ( have_posts() ) : 
-          while ( have_posts() ) : 
-            the_post();
-            the_content();
-          endwhile; 
-        else : 
-      ?>
-      <p>Sorry, no posts matched your criteria.</p>
-      <?php endif; ?>
     <div class="row">
       <div class="col s12 m1 l2 xl3"></div>
 
@@ -85,6 +225,7 @@ get_header();
 
 
   
+</main>
 
 <?php
 get_footer();
